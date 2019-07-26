40ff331bd1796dc30f3edcaf9d3480a2a3317b6b
SMacmanus <brinamac24@gmail.com>
2019-07-26 09:32:56 -0500

    3rd commit

--- GitPractice7_26/src/main/java/Main.java
+++ GitPractice7_26/src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1st commit");
         System.out.println("2nd commit");
+        System.out.println("3rd commit");
     }
 }
