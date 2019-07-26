fdcb7e3ee7a5075f27752627bd3879e12a0df260
SMacmanus <brinamac24@gmail.com>
2019-07-26 09:37:12 -0500

    6th commit

--- GitPractice7_26/src/main/java/Main.java
+++ GitPractice7_26/src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1st commit");
         System.out.println("2nd commit");
+        System.out.println("3rd commit");
+        System.out.println("4th commit");
+        System.out.println("5th commit");
     }
 }
