56432754d97841235015c57a784e4425fe7bff18
SMacmanus <brinamac24@gmail.com>
2019-07-26 09:33:40 -0500

    4th commit

--- GitPractice7_26/src/main/java/Main.java
+++ GitPractice7_26/src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("1st commit");
         System.out.println("2nd commit");
         System.out.println("3rd commit");
+        System.out.println("4th commit");
     }
 }
